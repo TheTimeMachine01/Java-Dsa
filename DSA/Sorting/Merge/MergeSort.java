1e6f5bbe862c2714a74506976a84d8d63d67d4a0
Ashish Verma <129376147+TheTimeMachine01@users.noreply.github.com>
2025-01-11 19:59:40 +0530

    Day 10 - 10th January 2025

--- DSA/Sorting/Merge/MergeSort.java
+++ DSA/Sorting/Merge/MergeSort.java
@@ -1,6 +1,6 @@
 package DSA.Sorting.Merge;
 
-public class MergeSort {
+public class  MergeSort {
     public static void main(String[] args) {
         int array[] = {9, 1, 4, 7, 8, 3, 6, 2, 5};
         int length = array.length;
